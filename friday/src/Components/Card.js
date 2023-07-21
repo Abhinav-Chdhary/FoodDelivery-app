e60b5aed637a608d1757a0ec12c9e8d4586f2eea
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-21 17:15:10 +0530

    Basic Home Page

---
+++ friday/src/Components/Card.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
+      <img className="card-img-top" src="..." alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">card's content.</p>
+        <div className="container w-100">
+          <select className="m-2 h-100 bg-success rounded">
+            {Array.from(Array(6), (e, i) => {
+              return (
+                <option key={i + 1} value={i + 1}>
+                  {i + 1}
+                </option>
+              );
+            })}
+          </select>
+          <select className="m-2 h-100 bg-success rounded">
+            <option>half</option>
+            <option>full</option>
+          </select>
+          <div className="d-inline fs-5">Price</div>
+        </div>
+      </div>
+    </div>
+  );
+}
