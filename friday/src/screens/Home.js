04b867f625017707a15d59bcfba4c340bcdbf42a
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-21 15:53:42 +0530

    Basic Frontend setup

---
+++ friday/src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
