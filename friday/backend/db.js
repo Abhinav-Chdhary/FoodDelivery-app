adbc4a6090c3e19108ec9a4a45220c646bd7ae87
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-25 12:50:01 +0530

    Resolved the fetch data error
    So earlier we were mixing the modern async await and older deprecated call back functionality which somehow did not work.
    No we are using only modern syntax.

--- friday/backend/db.js
+++ friday/backend/db.js
@@ -5,15 +5,17 @@ const mongouri =
 const mongoDB = async () => {
   try {
     await mongoose.connect(mongouri);
+    console.log("Connected to MongoDB");
     const fetched_data = await mongoose.connection.db.collection("food_items");
-    fetched_data.find().toArray(function (err, data) {
+    const finalFetchedData = await fetched_data.find().toArray();
+    console.log(finalFetchedData);
+    /* await fetched_data.find().toArray(function (err, data) {
       if (err) {
         console.error("Error retrieving data:", err);
       } else {
         console.log(data);
       }
-      console.log("Connected to MongoDB");
-    });
+    }); */
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
